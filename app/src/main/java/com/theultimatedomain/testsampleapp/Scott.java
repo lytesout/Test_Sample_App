672b8120e5507ffa9fd723832a7061bf180a3d58
STUDENT <student6@github.com>
2016-03-15 18:04:31 -0600

    added scott_activity.xml

---
+++ app/src/main/java/com/theultimatedomain/testsampleapp/Scott.java
@@ -0,0 +1,20 @@
+package com.theultimatedomain.testsampleapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by student6 on 3/15/16.
+ */
+public class Scott extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scott_activity);
+
+    }
+
+
+}

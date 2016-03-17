debae1727aa16c53f3204ec97062a6e664825fcd
STUDENT <student6@STEMs-MacBook-Air-3.local>
2016-03-17 16:07:28 -0600

    added scott_activity.xml

--- app/src/main/java/com/theultimatedomain/testsampleapp/Scott.java
+++ app/src/main/java/com/theultimatedomain/testsampleapp/Scott.java
@@ -2,6 +2,8 @@ package com.theultimatedomain.testsampleapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 
 /**
@@ -13,6 +15,30 @@ public class Scott extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scott_activity);
+
+    }
+
+    private Button btnLytesout;
+
+    private void setupUI()
+    {
+        btnLytesout = (Button) findViewById(R.id.btnLytesout);
+    }
+
+    private void setupClickListeners()
+    {
+        btnLytesout.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                lytesoff();
+            }
+        });
+    }
+
+    private void lytesoff()
+    {
 
     }
 

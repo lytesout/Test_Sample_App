dae0a748252dabaca04104058d3af5fc066f1272
John Doe <student2@STEMs-MacBook-Air-6.local>
2016-03-17 16:29:34 -0600

    Scott sucks.

---
+++ app/src/main/java/com/theultimatedomain/testsampleapp/Joe.java
@@ -0,0 +1,17 @@
+package com.theultimatedomain.testsampleapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by student2 on 3/15/16.
+ */
+public class Joe extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.joe);
+    }
+}
